c96a7950786df7b376a4c495346fe9cb2999c52f
Amogh <amogh.jonnalagadda@gmail.com>
2021-02-13 16:56:53 +0800

    first commit

---
+++ second.js
@@ -0,0 +1,53 @@
+true+true
+true
+
+true is uqual to 1
+false is uqual to 0
+
+true+true
+2
+true+false
+1
+true+10
+11
+true-10
+-9
+true*3
+3
+"h1"+true
+"h1true"
+true"-"true
+NaN
+true"+"true
+truetrue
+
+///////////////////////////
+
+=   >>> assignment
+==  >>> compare the data
+=== >>> compare data and data type
+
+var a = 10
+undefined
+var b = "10"
+undefined
+a==b
+TRUE
+a===b
+FALSE
+var c = true
+undefined
+var d = "true"
+undefined
+c == d
+FALSE
+c === d
+FALSE
+
+//case senstive
+var a = "hi"
+undefined
+var b = "Hi"
+undefined
+a==b
+false
---
+++ third.js
@@ -0,0 +1,60 @@
+console.log()  to check the output and debugging
+alert> notification
+
+alert("Form Submitted")
+alert("Task done")
+
+confirm
+confirm("Manaswi")
+true
+confirm("Amogh")
+false
+var a = confirm("Manaswi")
+undefined
+a
+true
+var a = confirm("Manaswi")
+undefined
+a
+false
+/////
+prompt>
+prompt("What is your name")
+"Amogh"
+var age =prompt("What is your age")
+undefined
+age
+"9"
+////////////
+var a = prompt("Please enter first number")
+var b = prompt("Please enter second number")
+alert(a+b)
+
+var a = "10"
+undefined
+parseInt(a)
+10
+var a = "hi"
+undefined
+parseInt(a)
+NaN
+var a = "10.888"
+undefined
+parseInt(a)
+10
+parseFloat(a)
+10.888
+var a = "10"
+undefined
+Number(a)
+10
+var b = "10.89"
+undefined
+Number(b)
+10.89
+
+var a = prompt("Please enter first number")
+var b = prompt("Please enter second number")
+alert(Number(a)+Number(b))
+
+var a: string = 'aaa'
